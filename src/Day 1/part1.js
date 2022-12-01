34f0cc2162f41a33eb985f170239c6d2825db970
Arron Chapman <unkwntech@gmail.com>
2022-12-01 07:52:46 -0600

    Day 1 Complete

---
+++ src/Day 1/part1.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let file = fs.readFileSync(`${__dirname}/input.txt`, "utf8");
+
+let input = file.split("\n");
+
+let elves = [];
+let calories = 0;
+
+for (let line of input) {
+    if (line.trim().length < 1) {
+        elves.push(calories);
+        calories = 0;
+    } else {
+        calories += parseInt(line.trim());
+    }
+}
+
+console.log(elves.sort((a, b) => (a < b ? 1 : -1))[0]);
