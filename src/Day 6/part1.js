88916ae2a83be5030631589a4f8efe523db9918c
Arron Chapman <unkwntech@gmail.com>
2022-12-06 07:44:10 -0600

    Day 6 solution

---
+++ src/Day 6/part1.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let input = fs.readFileSync(`${__dirname}/input.txt`, "utf8");
+
+let buffer = [];
+let startPos = 0;
+for (let char of input) {
+    while (buffer.includes(char)) {
+        buffer.shift();
+    }
+
+    buffer.push(char);
+
+    startPos++;
+
+    if (buffer.length == 14) break;
+}
+
+console.log(startPos);
